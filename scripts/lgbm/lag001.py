b22617c3d72890a24d467fcd76e2b482ca8f55c6
meltyyyyy <takeru.abe.g1@gmail.com>
2022-06-23 22:12:26 +0900

    format lag001.py

--- scripts/lgbm/lag001.py
+++ scripts/lgbm/lag001.py
@@ -6,6 +6,30 @@
 # In[50]:
 
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import StratifiedKFold
+from lightgbm import LGBMClassifier
+from tqdm.auto import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
+import multiprocessing
+import gc
+import sys
+import datetime
+import random
+import joblib
+import logging
+import shutil
+import warnings
+import json
+import os
+
+
 class Config:
     name = "LGBM/Lag001"
 
@@ -28,29 +52,10 @@ class Config:
 # In[51]:
 
 
-import os
-import json
-import warnings
-import shutil
-import logging
-import joblib
-import random
-import datetime
-import sys
-import gc
-import multiprocessing
-import joblib
-import pickle
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 # get_ipython().run_line_magic('matplotlib', 'inline')
 plt.style.use('seaborn-pastel')
-import seaborn as sns
 sns.set_palette("winter_r")
 
-from tqdm.auto import tqdm
 tqdm.pandas()
 warnings.filterwarnings('ignore')
 
@@ -75,7 +80,13 @@ for d in [INPUT, SUBMISSION, EXP_MODEL, EXP_FIG, EXP_PREDS]:
 
 
 train = pd.read_parquet(os.path.join(INPUT, 'train_small.parquet'))
-target = pd.read_csv(os.path.join(INPUT, 'train_labels.csv'), dtype={'customer_ID': 'str', 'target': 'int8'})
+target = pd.read_csv(
+    os.path.join(
+        INPUT,
+        'train_labels.csv'),
+    dtype={
+        'customer_ID': 'str',
+        'target': 'int8'})
 test = pd.read_parquet(os.path.join(INPUT, 'test_small.parquet'))
 
 
@@ -97,7 +108,8 @@ print(train.head())
 
 
 # @yunchonggan's fast metric implementation
-# From https://www.kaggle.com/competitions/amex-default-prediction/discussion/328020
+# From
+# https://www.kaggle.com/competitions/amex-default-prediction/discussion/328020
 def amex_metric(y_true: np.array, y_pred: np.array) -> float:
 
     # count of positives and negatives
@@ -128,6 +140,7 @@ def amex_metric(y_true: np.array, y_pred: np.array) -> float:
 
     return 0.5 * (g + d)
 
+
 def lgb_amex_metric(y_true, y_pred):
     """The competition metric with lightgbm's calling convention"""
     return ('amex',
@@ -144,7 +157,7 @@ def plot_importances(models, fig):
     importance_df = pd.DataFrame(models[0].feature_importances_,
                                  index=features,
                                  columns=['importance'])\
-                        .sort_values("importance", ascending=False)
+        .sort_values("importance", ascending=False)
 
     plt.subplots(figsize=(len(features) // 4, 5))
     plt.bar(importance_df.index, importance_df.importance)
@@ -158,10 +171,6 @@ def plot_importances(models, fig):
 # ## Model
 
 # In[58]:
-
-
-from lightgbm import LGBMClassifier
-from sklearn.model_selection import StratifiedKFold
 
 
 def fit_lgbm(X, y, params=None):
@@ -217,8 +226,20 @@ def inference_lgbm(models, X):
 # In[59]:
 
 
-cat_features = ['B_30', 'B_38', 'D_114', 'D_116', 'D_117', 'D_120', 'D_126', 'D_63', 'D_64', 'D_66', 'D_68']
-cont_features = [col for col in train.columns if col not in cat_features + [Config.target, 'S_2', 'customer_ID']]
+cat_features = [
+    'B_30',
+    'B_38',
+    'D_114',
+    'D_116',
+    'D_117',
+    'D_120',
+    'D_126',
+    'D_63',
+    'D_64',
+    'D_66',
+    'D_68']
+cont_features = [col for col in train.columns if col not in cat_features +
+                 [Config.target, 'S_2', 'customer_ID']]
 
 
 # In[60]:
@@ -228,8 +249,9 @@ def _add_diff_features(args, step=3):
     customer_id, df = args
     dfs = []
     for i in range(step):
-        shift = i+1
-        df_diff = df[cont_features].diff(shift).rename(columns={f: f"{f}_diff{shift}" for f in cont_features})
+        shift = i + 1
+        df_diff = df[cont_features].diff(shift).rename(
+            columns={f: f"{f}_diff{shift}" for f in cont_features})
         df_diff = df_diff.tail(1).reset_index(drop=True)
         dfs.append(df_diff)
     df = pd.concat(dfs, axis=1)
@@ -237,26 +259,28 @@ def _add_diff_features(args, step=3):
     return df
 
 
-def add_diff_features(df : pd.DataFrame, processes=32):
+def add_diff_features(df: pd.DataFrame, processes=32):
     with multiprocessing.Pool(processes=processes) as pool:
-        dfs = pool.imap_unordered(_add_diff_features, df.groupby('customer_ID'))
+        dfs = pool.imap_unordered(
+            _add_diff_features,
+            df.groupby('customer_ID'))
         dfs = list(dfs)
     df = pd.concat(dfs)
     return df.reset_index(drop=True).sort_index(axis=1)
 
-train_diff = add_diff_features(train.copy()).merge(target, how='left', on='customer_ID')
-# test_diff = add_diff_features(test.copy())
 
+train_diff = add_diff_features(
+    train.copy()).merge(
+        target,
+        how='left',
+    on='customer_ID')
+# test_diff = add_diff_features(test.copy())
 
 
 # In[64]:
 
 
-from sklearn.linear_model import Lasso
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import StandardScaler
-
-def select_cont_features(df : pd.DataFrame, features, target, max_features=150):
+def select_cont_features(df: pd.DataFrame, features, target, max_features=150):
     features_list = []
     train_y = df[target]
     train_X = df[features]
@@ -268,7 +292,10 @@ def select_cont_features(df : pd.DataFrame, features, target, max_features=150):
     scaler = StandardScaler()
     scaler.fit(train_X)
     for alpha in [3e-3, 5e-3, 7e-3, 1e-2, 3e-2]:
-        selector = SelectFromModel(Lasso(alpha=alpha), max_features=max_features)
+        selector = SelectFromModel(
+            Lasso(
+                alpha=alpha),
+            max_features=max_features)
         selector.fit(scaler.transform(train_X), train_y)
         selected_features = train_X.columns.values[selector.get_support()]
         features_list.append(selected_features)
@@ -276,7 +303,12 @@ def select_cont_features(df : pd.DataFrame, features, target, max_features=150):
 
     return features_list
 
-def select_shift_features(df : pd.DataFrame, features, target, max_features=150):
+
+def select_shift_features(
+        df: pd.DataFrame,
+        features,
+        target,
+        max_features=150):
     shift_features_list = []
     train_y = df[target]
     train_X = df[features]
@@ -298,7 +330,10 @@ def select_shift_features(df : pd.DataFrame, features, target, max_features=150)
     scaler = StandardScaler()
     scaler.fit(train_X)
     for alpha in [7e-3, 1e-2, 3e-2, 1e-1]:
-        selector = SelectFromModel(Lasso(alpha=alpha), max_features=max_features)
+        selector = SelectFromModel(
+            Lasso(
+                alpha=alpha),
+            max_features=max_features)
         selector.fit(scaler.transform(train_X), train_y)
         selected_features = train_X.columns.values[selector.get_support()]
         selected_features = np.hstack((selected_features, cat_shift_features))
@@ -307,19 +342,492 @@ def select_shift_features(df : pd.DataFrame, features, target, max_features=150)
 
     return shift_features_list
 
-print('======= select diff features =======')
-features = [col for col in train_diff.columns if col not in ['customer_ID', Config.target]]
-diff_features_list = select_cont_features(train_diff, features, Config.target)
 
+print('======= select diff features =======')
+features = [
+    col for col in train_diff.columns if col not in [
+        'customer_ID',
+        Config.target]]
+diff_features_list = select_cont_features(train_diff, features, Config.target)
 
 
 # In[65]:
 
 
-features_avg = ['B_1', 'B_2', 'B_3', 'B_4', 'B_5', 'B_6', 'B_8', 'B_9', 'B_10', 'B_11', 'B_12', 'B_13', 'B_14', 'B_15', 'B_16', 'B_17', 'B_18', 'B_19', 'B_20', 'B_21', 'B_22', 'B_23', 'B_24', 'B_25', 'B_28', 'B_29', 'B_30', 'B_32', 'B_33', 'B_37', 'B_38', 'B_39', 'B_40', 'B_41', 'B_42', 'D_39', 'D_41', 'D_42', 'D_43', 'D_44', 'D_45', 'D_46', 'D_47', 'D_48', 'D_50', 'D_51', 'D_53', 'D_54', 'D_55', 'D_58', 'D_59', 'D_60', 'D_61', 'D_62', 'D_65', 'D_66', 'D_69', 'D_70', 'D_71', 'D_72', 'D_73', 'D_74', 'D_75', 'D_76', 'D_77', 'D_78', 'D_80', 'D_82', 'D_84', 'D_86', 'D_91', 'D_92', 'D_94', 'D_96', 'D_103', 'D_104', 'D_108', 'D_112', 'D_113', 'D_114', 'D_115', 'D_117', 'D_118', 'D_119', 'D_120', 'D_121', 'D_122', 'D_123', 'D_124', 'D_125', 'D_126', 'D_128', 'D_129', 'D_131', 'D_132', 'D_133', 'D_134', 'D_135', 'D_136', 'D_140', 'D_141', 'D_142', 'D_144', 'D_145', 'P_2', 'P_3', 'P_4', 'R_1', 'R_2', 'R_3', 'R_7', 'R_8', 'R_9', 'R_10', 'R_11', 'R_14', 'R_15', 'R_16', 'R_17', 'R_20', 'R_21', 'R_22', 'R_24', 'R_26', 'R_27', 'S_3', 'S_5', 'S_6', 'S_7', 'S_9', 'S_11', 'S_12', 'S_13', 'S_15', 'S_16', 'S_18', 'S_22', 'S_23', 'S_25', 'S_26']
-features_min = ['B_2', 'B_4', 'B_5', 'B_9', 'B_13', 'B_14', 'B_15', 'B_16', 'B_17', 'B_19', 'B_20', 'B_28', 'B_29', 'B_33', 'B_36', 'B_42', 'D_39', 'D_41', 'D_42', 'D_45', 'D_46', 'D_48', 'D_50', 'D_51', 'D_53', 'D_55', 'D_56', 'D_58', 'D_59', 'D_60', 'D_62', 'D_70', 'D_71', 'D_74', 'D_75', 'D_78', 'D_83', 'D_102', 'D_112', 'D_113', 'D_115', 'D_118', 'D_119', 'D_121', 'D_122', 'D_128', 'D_132', 'D_140', 'D_141', 'D_144', 'D_145', 'P_2', 'P_3', 'R_1', 'R_27', 'S_3', 'S_5', 'S_7', 'S_9', 'S_11', 'S_12', 'S_23', 'S_25']
-features_max = ['B_1', 'B_2', 'B_3', 'B_4', 'B_5', 'B_6', 'B_7', 'B_8', 'B_9', 'B_10', 'B_12', 'B_13', 'B_14', 'B_15', 'B_16', 'B_17', 'B_18', 'B_19', 'B_21', 'B_23', 'B_24', 'B_25', 'B_29', 'B_30', 'B_33', 'B_37', 'B_38', 'B_39', 'B_40', 'B_42', 'D_39', 'D_41', 'D_42', 'D_43', 'D_44', 'D_45', 'D_46', 'D_47', 'D_48', 'D_49', 'D_50', 'D_52', 'D_55', 'D_56', 'D_58', 'D_59', 'D_60', 'D_61', 'D_63', 'D_64', 'D_65', 'D_70', 'D_71', 'D_72', 'D_73', 'D_74', 'D_76', 'D_77', 'D_78', 'D_80', 'D_82', 'D_84', 'D_91', 'D_102', 'D_105', 'D_107', 'D_110', 'D_111', 'D_112', 'D_115', 'D_116', 'D_117', 'D_118', 'D_119', 'D_121', 'D_122', 'D_123', 'D_124', 'D_125', 'D_126', 'D_128', 'D_131', 'D_132', 'D_133', 'D_134', 'D_135', 'D_136', 'D_138', 'D_140', 'D_141', 'D_142', 'D_144', 'D_145', 'P_2', 'P_3', 'P_4', 'R_1', 'R_3', 'R_5', 'R_6', 'R_7', 'R_8', 'R_10', 'R_11', 'R_14', 'R_17', 'R_20', 'R_26', 'R_27', 'S_3', 'S_5', 'S_7', 'S_8', 'S_11', 'S_12', 'S_13', 'S_15', 'S_16', 'S_22', 'S_23', 'S_24', 'S_25', 'S_26', 'S_27']
-features_last = ['B_1', 'B_2', 'B_3', 'B_4', 'B_5', 'B_6', 'B_7', 'B_8', 'B_9', 'B_10', 'B_11', 'B_12', 'B_13', 'B_14', 'B_15', 'B_16', 'B_17', 'B_18', 'B_19', 'B_20', 'B_21', 'B_22', 'B_23', 'B_24', 'B_25', 'B_26', 'B_28', 'B_29', 'B_30', 'B_32', 'B_33', 'B_36', 'B_37', 'B_38', 'B_39', 'B_40', 'B_41', 'B_42', 'D_39', 'D_41', 'D_42', 'D_43', 'D_44', 'D_45', 'D_46', 'D_47', 'D_48', 'D_49', 'D_50', 'D_51', 'D_52', 'D_53', 'D_54', 'D_55', 'D_56', 'D_58', 'D_59', 'D_60', 'D_61', 'D_62', 'D_63', 'D_64', 'D_65', 'D_69', 'D_70', 'D_71', 'D_72', 'D_73', 'D_75', 'D_76', 'D_77', 'D_78', 'D_79', 'D_80', 'D_81', 'D_82', 'D_83', 'D_86', 'D_91', 'D_96', 'D_105', 'D_106', 'D_112', 'D_114', 'D_119', 'D_120', 'D_121', 'D_122', 'D_124', 'D_125', 'D_126', 'D_127', 'D_130', 'D_131', 'D_132', 'D_133', 'D_134', 'D_138', 'D_140', 'D_141', 'D_142', 'D_145', 'P_2', 'P_3', 'P_4', 'R_1', 'R_2', 'R_3', 'R_4', 'R_5', 'R_6', 'R_7', 'R_8', 'R_9', 'R_10', 'R_11', 'R_12', 'R_13', 'R_14', 'R_15', 'R_19', 'R_20', 'R_26', 'R_27', 'S_3', 'S_5', 'S_6', 'S_7', 'S_8', 'S_9', 'S_11', 'S_12', 'S_13', 'S_16', 'S_19', 'S_20', 'S_22', 'S_23', 'S_24', 'S_25', 'S_26', 'S_27']
+features_avg = [
+    'B_1',
+    'B_2',
+    'B_3',
+    'B_4',
+    'B_5',
+    'B_6',
+    'B_8',
+    'B_9',
+    'B_10',
+    'B_11',
+    'B_12',
+    'B_13',
+    'B_14',
+    'B_15',
+    'B_16',
+    'B_17',
+    'B_18',
+    'B_19',
+    'B_20',
+    'B_21',
+    'B_22',
+    'B_23',
+    'B_24',
+    'B_25',
+    'B_28',
+    'B_29',
+    'B_30',
+    'B_32',
+    'B_33',
+    'B_37',
+    'B_38',
+    'B_39',
+    'B_40',
+    'B_41',
+    'B_42',
+    'D_39',
+    'D_41',
+    'D_42',
+    'D_43',
+    'D_44',
+    'D_45',
+    'D_46',
+    'D_47',
+    'D_48',
+    'D_50',
+    'D_51',
+    'D_53',
+    'D_54',
+    'D_55',
+    'D_58',
+    'D_59',
+    'D_60',
+    'D_61',
+    'D_62',
+    'D_65',
+    'D_66',
+    'D_69',
+    'D_70',
+    'D_71',
+    'D_72',
+    'D_73',
+    'D_74',
+    'D_75',
+    'D_76',
+    'D_77',
+    'D_78',
+    'D_80',
+    'D_82',
+    'D_84',
+    'D_86',
+    'D_91',
+    'D_92',
+    'D_94',
+    'D_96',
+    'D_103',
+    'D_104',
+    'D_108',
+    'D_112',
+    'D_113',
+    'D_114',
+    'D_115',
+    'D_117',
+    'D_118',
+    'D_119',
+    'D_120',
+    'D_121',
+    'D_122',
+    'D_123',
+    'D_124',
+    'D_125',
+    'D_126',
+    'D_128',
+    'D_129',
+    'D_131',
+    'D_132',
+    'D_133',
+    'D_134',
+    'D_135',
+    'D_136',
+    'D_140',
+    'D_141',
+    'D_142',
+    'D_144',
+    'D_145',
+    'P_2',
+    'P_3',
+    'P_4',
+    'R_1',
+    'R_2',
+    'R_3',
+    'R_7',
+    'R_8',
+    'R_9',
+    'R_10',
+    'R_11',
+    'R_14',
+    'R_15',
+    'R_16',
+    'R_17',
+    'R_20',
+    'R_21',
+    'R_22',
+    'R_24',
+    'R_26',
+    'R_27',
+    'S_3',
+    'S_5',
+    'S_6',
+    'S_7',
+    'S_9',
+    'S_11',
+    'S_12',
+    'S_13',
+    'S_15',
+    'S_16',
+    'S_18',
+    'S_22',
+    'S_23',
+    'S_25',
+    'S_26']
+features_min = [
+    'B_2',
+    'B_4',
+    'B_5',
+    'B_9',
+    'B_13',
+    'B_14',
+    'B_15',
+    'B_16',
+    'B_17',
+    'B_19',
+    'B_20',
+    'B_28',
+    'B_29',
+    'B_33',
+    'B_36',
+    'B_42',
+    'D_39',
+    'D_41',
+    'D_42',
+    'D_45',
+    'D_46',
+    'D_48',
+    'D_50',
+    'D_51',
+    'D_53',
+    'D_55',
+    'D_56',
+    'D_58',
+    'D_59',
+    'D_60',
+    'D_62',
+    'D_70',
+    'D_71',
+    'D_74',
+    'D_75',
+    'D_78',
+    'D_83',
+    'D_102',
+    'D_112',
+    'D_113',
+    'D_115',
+    'D_118',
+    'D_119',
+    'D_121',
+    'D_122',
+    'D_128',
+    'D_132',
+    'D_140',
+    'D_141',
+    'D_144',
+    'D_145',
+    'P_2',
+    'P_3',
+    'R_1',
+    'R_27',
+    'S_3',
+    'S_5',
+    'S_7',
+    'S_9',
+    'S_11',
+    'S_12',
+    'S_23',
+    'S_25']
+features_max = [
+    'B_1',
+    'B_2',
+    'B_3',
+    'B_4',
+    'B_5',
+    'B_6',
+    'B_7',
+    'B_8',
+    'B_9',
+    'B_10',
+    'B_12',
+    'B_13',
+    'B_14',
+    'B_15',
+    'B_16',
+    'B_17',
+    'B_18',
+    'B_19',
+    'B_21',
+    'B_23',
+    'B_24',
+    'B_25',
+    'B_29',
+    'B_30',
+    'B_33',
+    'B_37',
+    'B_38',
+    'B_39',
+    'B_40',
+    'B_42',
+    'D_39',
+    'D_41',
+    'D_42',
+    'D_43',
+    'D_44',
+    'D_45',
+    'D_46',
+    'D_47',
+    'D_48',
+    'D_49',
+    'D_50',
+    'D_52',
+    'D_55',
+    'D_56',
+    'D_58',
+    'D_59',
+    'D_60',
+    'D_61',
+    'D_63',
+    'D_64',
+    'D_65',
+    'D_70',
+    'D_71',
+    'D_72',
+    'D_73',
+    'D_74',
+    'D_76',
+    'D_77',
+    'D_78',
+    'D_80',
+    'D_82',
+    'D_84',
+    'D_91',
+    'D_102',
+    'D_105',
+    'D_107',
+    'D_110',
+    'D_111',
+    'D_112',
+    'D_115',
+    'D_116',
+    'D_117',
+    'D_118',
+    'D_119',
+    'D_121',
+    'D_122',
+    'D_123',
+    'D_124',
+    'D_125',
+    'D_126',
+    'D_128',
+    'D_131',
+    'D_132',
+    'D_133',
+    'D_134',
+    'D_135',
+    'D_136',
+    'D_138',
+    'D_140',
+    'D_141',
+    'D_142',
+    'D_144',
+    'D_145',
+    'P_2',
+    'P_3',
+    'P_4',
+    'R_1',
+    'R_3',
+    'R_5',
+    'R_6',
+    'R_7',
+    'R_8',
+    'R_10',
+    'R_11',
+    'R_14',
+    'R_17',
+    'R_20',
+    'R_26',
+    'R_27',
+    'S_3',
+    'S_5',
+    'S_7',
+    'S_8',
+    'S_11',
+    'S_12',
+    'S_13',
+    'S_15',
+    'S_16',
+    'S_22',
+    'S_23',
+    'S_24',
+    'S_25',
+    'S_26',
+    'S_27']
+features_last = [
+    'B_1',
+    'B_2',
+    'B_3',
+    'B_4',
+    'B_5',
+    'B_6',
+    'B_7',
+    'B_8',
+    'B_9',
+    'B_10',
+    'B_11',
+    'B_12',
+    'B_13',
+    'B_14',
+    'B_15',
+    'B_16',
+    'B_17',
+    'B_18',
+    'B_19',
+    'B_20',
+    'B_21',
+    'B_22',
+    'B_23',
+    'B_24',
+    'B_25',
+    'B_26',
+    'B_28',
+    'B_29',
+    'B_30',
+    'B_32',
+    'B_33',
+    'B_36',
+    'B_37',
+    'B_38',
+    'B_39',
+    'B_40',
+    'B_41',
+    'B_42',
+    'D_39',
+    'D_41',
+    'D_42',
+    'D_43',
+    'D_44',
+    'D_45',
+    'D_46',
+    'D_47',
+    'D_48',
+    'D_49',
+    'D_50',
+    'D_51',
+    'D_52',
+    'D_53',
+    'D_54',
+    'D_55',
+    'D_56',
+    'D_58',
+    'D_59',
+    'D_60',
+    'D_61',
+    'D_62',
+    'D_63',
+    'D_64',
+    'D_65',
+    'D_69',
+    'D_70',
+    'D_71',
+    'D_72',
+    'D_73',
+    'D_75',
+    'D_76',
+    'D_77',
+    'D_78',
+    'D_79',
+    'D_80',
+    'D_81',
+    'D_82',
+    'D_83',
+    'D_86',
+    'D_91',
+    'D_96',
+    'D_105',
+    'D_106',
+    'D_112',
+    'D_114',
+    'D_119',
+    'D_120',
+    'D_121',
+    'D_122',
+    'D_124',
+    'D_125',
+    'D_126',
+    'D_127',
+    'D_130',
+    'D_131',
+    'D_132',
+    'D_133',
+    'D_134',
+    'D_138',
+    'D_140',
+    'D_141',
+    'D_142',
+    'D_145',
+    'P_2',
+    'P_3',
+    'P_4',
+    'R_1',
+    'R_2',
+    'R_3',
+    'R_4',
+    'R_5',
+    'R_6',
+    'R_7',
+    'R_8',
+    'R_9',
+    'R_10',
+    'R_11',
+    'R_12',
+    'R_13',
+    'R_14',
+    'R_15',
+    'R_19',
+    'R_20',
+    'R_26',
+    'R_27',
+    'S_3',
+    'S_5',
+    'S_6',
+    'S_7',
+    'S_8',
+    'S_9',
+    'S_11',
+    'S_12',
+    'S_13',
+    'S_16',
+    'S_19',
+    'S_20',
+    'S_22',
+    'S_23',
+    'S_24',
+    'S_25',
+    'S_26',
+    'S_27']
+
 
 def add_features(df):
     cid = pd.Categorical(df.pop('customer_ID'), ordered=True)
@@ -328,28 +836,29 @@ def add_features(df):
               .groupby(cid)
               .mean()[features_avg]
               .rename(columns={f: f"{f}_avg" for f in features_avg})
-             )
+              )
     gc.collect()
     df_min = (df
               .groupby(cid)
               .min()[features_min]
               .rename(columns={f: f"{f}_min" for f in features_min})
-             )
+              )
     gc.collect()
     df_max = (df
               .groupby(cid)
               .max()[features_max]
               .rename(columns={f: f"{f}_max" for f in features_max})
-             )
+              )
     gc.collect()
     df = (df.loc[last, features_last]
           .rename(columns={f: f"{f}_last" for f in features_last})
           .set_index(np.asarray(cid[last]))
-         )
+          )
     gc.collect()
     df = pd.concat([df, df_min, df_max, df_avg], axis=1)
     del df_avg, df_min, df_max, cid, last
     return df
+
 
 train = add_features(train).join(target.set_index('customer_ID'), how='left')
 # test = add_features(test)
@@ -373,15 +882,13 @@ lgb_params = {"learning_rate": 0.03,
               'reg_lambda': 50,
               'min_child_samples': 2400,
               'num_leaves': 95,
-              "metric" : "None",
+              "metric": "None",
               'max_bin': 511}
 
 print('======= Diff Features =======')
 for diff_features in diff_features_list:
-    data = train.join(train_diff[list(diff_features) + ['customer_ID']].set_index('customer_ID'), how='left')
+    data = train.join(train_diff[list(diff_features) +
+                                 ['customer_ID']].set_index('customer_ID'), how='left')
     features = get_faetures(data)
     fit_lgbm(data[features], data[Config.target], params=lgb_params)
 del train_diff
-
-
-
